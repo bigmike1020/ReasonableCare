6905e99b63d6958576bf89e73ba940d5a4a58461
losmescaleros <mdnevill@ncsu.edu>
2014-03-21 12:21:29 -0400

    Initial check in

---
+++ ReasonableCare/src/ReasonableCare.java
@@ -0,0 +1,14 @@
+/**
+ * Entrance program for the Reasonable Care system
+ */
+public class ReasonableCare {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world");
+	}
+
+}
