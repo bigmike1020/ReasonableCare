cc3bbdd37d9e2d4fd4b40b742b9432f77ab48795
Mike Senn <mike@msenn.com>
2014-03-23 23:11:52 -0400

    Create staff shell with insert student method
    not tested

---
+++ ReasonableCare/src/StaffShell.java
@@ -0,0 +1,38 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import asg.cliche.Command;
+
+public class StaffShell {
+
+  /**
+   * Connection object used to create Statements. This shell doesn't own the
+   * connection; no need to close.
+   */
+  final Connection connection;
+
+  public StaffShell(Connection connection) {
+    this.connection = connection;
+  }
+
+  @Command
+  public Object createStudent(String name, String password,
+      Timestamp startingDate) throws SQLException {
+
+    String sql = "insert into student(studentName,password,startingDate) values(?,?,?)";
+
+    // Create a statement instance that will be sending
+    // your SQL statements to the DBMS
+    try (PreparedStatement stm = connection.prepareStatement(sql)) {
+
+      stm.setString(1, name);
+      stm.setString(2, password);
+      stm.setTimestamp(3, startingDate);
+      int id = stm.executeUpdate();
+      
+      return "Created new student with id " + id;
+    }
+  }
+}
