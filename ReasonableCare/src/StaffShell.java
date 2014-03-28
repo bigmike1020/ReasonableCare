91d2f054b3558ecb7c38ecdd3be90627b1fe9aab
Glenn Setliff Jr. <glsetliff1@gmail.com>
2014-03-28 18:04:46 -0400

    Updated create-student to return studentID

--- ReasonableCare/src/StaffShell.java
+++ ReasonableCare/src/StaffShell.java
@@ -1,8 +1,8 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 //import java.sql.Timestamp;
-//import java.sql.Date;
 
 import asg.cliche.Command;
 
@@ -26,14 +26,23 @@ public class StaffShell {
 
     // Create a statement instance that will be sending
     // your SQL statements to the DBMS
-    try (PreparedStatement stm = connection.prepareStatement(sql)) {
+    // second argument is the generated key that is to be returned
+    try (PreparedStatement stm = connection.prepareStatement(sql, new String[]
+    		{"StudentID"})) {
     	
       stm.setString(1, name);
       stm.setString(2, password);
       stm.setDate(3, java.sql.Date.valueOf(startingDate)); //date must be in YYYY-MM-DD format
-      int id = stm.executeUpdate();
+      stm.executeUpdate();
       
-      return "Created new student with id " + id;
+      //get the auto-generated key from the row that was added
+      int id=0;
+      
+      ResultSet rs = stm.getGeneratedKeys();
+      if (rs != null && rs.next()) {
+          id = rs.getInt(1);}
+      
+      return "Created new student with id "+ id;
     }
   }
 }
