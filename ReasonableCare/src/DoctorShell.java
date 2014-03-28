39ea6f5a8ad17e5f5eb0a92ebc9399aded7853e3
Glenn Setliff Jr. <glsetliff1@gmail.com>
2014-03-28 11:59:51 -0400

    Testing the use of egit and creating a blank doctorshell class

--- ReasonableCare/src/DoctorShell.java
+++ ReasonableCare/src/DoctorShell.java
@@ -1,6 +1,12 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 
 public class DoctorShell {
 	
 	//test for committing code using eclipse
+	
+	
 
 }
