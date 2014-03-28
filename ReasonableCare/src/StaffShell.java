77ebb6eca1f11a0cab4ead07c70a05b6c8048cdf
Glenn Setliff Jr. <glsetliff1@gmail.com>
2014-03-28 14:57:24 -0400

    @command create-student - Changed format of startingDate from timestamp to date and converted entered string to date.
    Confirmed creation of student in DB, but function returned 1 as ID and
    created student in DB with id of 1040

--- ReasonableCare/src/StaffShell.java
+++ ReasonableCare/src/StaffShell.java
@@ -1,7 +1,8 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Timestamp;
+//import java.sql.Timestamp;
+//import java.sql.Date;
 
 import asg.cliche.Command;
 
@@ -19,17 +20,17 @@ public class StaffShell {
 
   @Command
   public Object createStudent(String name, String password,
-      Timestamp startingDate) throws SQLException {
+      String startingDate) throws SQLException {
 
     String sql = "insert into student(studentName,password,startingDate) values(?,?,?)";
 
     // Create a statement instance that will be sending
     // your SQL statements to the DBMS
     try (PreparedStatement stm = connection.prepareStatement(sql)) {
-
+    	
       stm.setString(1, name);
       stm.setString(2, password);
-      stm.setTimestamp(3, startingDate);
+      stm.setDate(3, java.sql.Date.valueOf(startingDate));
       int id = stm.executeUpdate();
       
       return "Created new student with id " + id;
