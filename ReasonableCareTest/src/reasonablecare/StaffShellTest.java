443709827c66579ba1d5bc1f89649174667057ec
Mike Senn <mike@msenn.com>
2014-03-29 08:52:24 -0400

    Use test DB in test

--- ReasonableCareTest/src/reasonablecare/StaffShellTest.java
+++ ReasonableCareTest/src/reasonablecare/StaffShellTest.java
@@ -1,46 +1,66 @@
 package reasonablecare;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
+import java.io.FileInputStream;
+import java.sql.Connection;
 import java.sql.SQLException;
 
 import org.dbunit.DBTestCase;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
-import org.junit.*;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class StaffShellTest extends DBTestCase {
 
+  Connection connection;
   StaffShell shell;
-  
+
+  public StaffShellTest(String name) {
+    super(name);
+    System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS,
+        Constants.driverClass);
+    System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL,
+        Constants.jdbcURL);
+    System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME,
+        Constants.user);
+    System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD,
+        Constants.password);
+    // System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_SCHEMA, ""
+    // );
+  }
+
   @Before
   protected void setUp() throws Exception {
     super.setUp();
-    
-    shell = null;
+    shell = new StaffShell(connection = Constants.makeConnection());
   }
 
   @After
   protected void tearDown() throws Exception {
-
     shell = null;
-    
+    connection.close();
+    connection = null;
     super.tearDown();
   }
 
   @Override
   protected IDataSet getDataSet() throws Exception {
-    // TODO Auto-generated method stub
-    return null;
+    return new FlatXmlDataSetBuilder().build(new FileInputStream(
+        "db/shell1.xml"));
   }
-  
+
   @Test
   public void testGetDoctors() throws SQLException {
     Object result = shell.getDoctors();
     Object expected = "List of all DoctorIDs\n0: 4000\n";
-    
+
     assertThat(result, equalTo(expected));
   }
-
 
 }
