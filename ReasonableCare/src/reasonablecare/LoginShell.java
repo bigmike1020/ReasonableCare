4e16dea2284e6a095cba1369d42a99d0810914f0
Mike Senn <mike@msenn.com>
2014-03-30 13:20:17 -0400

    Add password check functionality

--- ReasonableCare/src/reasonablecare/LoginShell.java
+++ ReasonableCare/src/reasonablecare/LoginShell.java
@@ -1,13 +1,22 @@
 package reasonablecare;
 
-import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import asg.cliche.Command;
 import asg.cliche.Shell;
 import asg.cliche.ShellDependent;
 
 public class LoginShell implements ShellDependent {
+
+  private static class LoginFailedException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public LoginFailedException(String msg) {
+      super(msg);
+    }
+  }
 
   /**
    * Connection object used to create Statements. This shell doesn't own the
@@ -38,49 +47,110 @@ public class LoginShell implements ShellDependent {
   }
 
   @Command
-  public String login(int id, String password) throws IOException {
-    if (1000 <= id && id < 2000) {
-      loginStudent(id, password);
-    } else if (2000 <= id && id < 3000) {
-      loginDoctor(id, password);
-    } else if (4000 <= id && id < 5000) {
-      loginNurse(id, password);
-    } else if (5000 <= id && id < 6000) {
-      loginStaff(id, password);
-    } else {
-      return "Invalid id. Not in any user class range.";
+  public String login(int id, String password) throws Exception {
+
+    try {
+      if (1000 <= id && id < 2000) {
+        loginStudent(id, password);
+      } else if (2000 <= id && id < 3000) {
+        loginDoctor(id, password);
+      } else if (4000 <= id && id < 5000) {
+        loginNurse(id, password);
+      } else if (5000 <= id && id < 6000) {
+        loginStaff(id, password);
+      } else {
+        return "Invalid id. Not in any user class range.";
+      }
+    } catch (LoginFailedException e) {
+      return e.getMessage();
     }
-    
+
     return "Back at login shell.";
   }
 
-  private void loginStudent(int id, String password) throws IOException {
+  private void loginStudent(int id, String password) throws Exception {
 
-    // TODO verify id/password is correct
+    String sql = "select 1 from Student where StudentID=? and password=?";
+
+    // Create a statement instance that will be sending
+    // your SQL statements to the DBMS
+    try (PreparedStatement stm = connection.prepareStatement(sql)) {
+
+      stm.setInt(1, id);
+      stm.setString(2, password);
+
+      ResultSet rs = stm.executeQuery();
+      if (!rs.next()) {
+        throw new LoginFailedException("Invalid student ID or password");
+      }
+
+    }
 
     factory.createSubshell(loginShell, "student",
         new StudentShell(connection, id)).commandLoop();
   }
 
-  private void loginStaff(int id, String password) throws IOException {
+  private void loginStaff(int id, String password) throws Exception {
 
-    // TODO verify id/password is correct
+    String sql = "select 1 from Staff where staffID=? and password=?";
+
+    // Create a statement instance that will be sending
+    // your SQL statements to the DBMS
+    try (PreparedStatement stm = connection.prepareStatement(sql)) {
+
+      stm.setInt(1, id);
+      stm.setString(2, password);
+
+      ResultSet rs = stm.executeQuery();
+      if (!rs.next()) {
+        throw new LoginFailedException("Invalid staff ID or password");
+      }
+
+    }
 
     factory.createSubshell(loginShell, "staff", new StaffShell(connection, id))
         .commandLoop();
   }
 
-  private void loginDoctor(int id, String password) throws IOException {
+  private void loginDoctor(int id, String password) throws Exception {
 
-    // TODO verify id/password is correct
+    String sql = "select 1 from Doctor where doctorID=? and password=?";
+
+    // Create a statement instance that will be sending
+    // your SQL statements to the DBMS
+    try (PreparedStatement stm = connection.prepareStatement(sql)) {
+
+      stm.setInt(1, id);
+      stm.setString(2, password);
+
+      ResultSet rs = stm.executeQuery();
+      if (!rs.next()) {
+        throw new LoginFailedException("Invalid doctor ID or password");
+      }
+
+    }
 
     factory.createSubshell(loginShell, "doctor",
         new DoctorShell(connection, id)).commandLoop();
   }
 
-  private void loginNurse(int id, String password) throws IOException {
+  private void loginNurse(int id, String password) throws Exception {
 
-    // TODO verify id/password is correct
+    String sql = "select 1 from Nurse where NurseID=? and password=?";
+
+    // Create a statement instance that will be sending
+    // your SQL statements to the DBMS
+    try (PreparedStatement stm = connection.prepareStatement(sql)) {
+
+      stm.setInt(1, id);
+      stm.setString(2, password);
+
+      ResultSet rs = stm.executeQuery();
+      if (!rs.next()) {
+        throw new LoginFailedException("Invalid nurse ID or password");
+      }
+
+    }
 
     factory.createSubshell(loginShell, "nurse", new NurseShell(connection, id))
         .commandLoop();
