c0a655baac46752223f46423b673008eb2ba2768
Mike Senn <mike@msenn.com>
2014-04-08 19:20:16 -0400

    Implement checkFutureAppointments for doctors

--- ReasonableCare/src/reasonablecare/DoctorShell.java
+++ ReasonableCare/src/reasonablecare/DoctorShell.java
@@ -47,8 +47,25 @@ public class DoctorShell {
 	}
 
 	@Command
-	public void checkFutureAppointments() {
-		// TODO list future appointments
+	public Table checkFutureAppointments() throws SQLException {
+		String sql = "select appointmentId, studentName, appointmenttime, type, reasonforvisit, doctornotes "
+				+ "FROM Appointment join makesappointment using(appointmentid) join student using(studentid) "
+				+ "WHERE doctorid=? AND (appointmenttime > CURRENT_TIMESTAMP)";
+
+		// Create a statement to enter staff into DB and return ID
+		try (PreparedStatement stm = connection.prepareStatement(sql)) {
+			stm.setInt(1, id);
+
+			Table table = new Table("Appt ID", "Student", "Date and Time",
+					"Appt Type", "Appt Reason", "Notes");
+			ResultSet rs = stm.executeQuery();
+			while (rs.next()) {
+				table.add(rs.getInt(1), rs.getString(2), rs.getTimestamp(3),
+						rs.getString(4), rs.getString(5), rs.getString(6));
+			}
+
+			return table;
+		}
 	}
 
 	@Command
