b1a80bbab08f2cdfaab4a1ea7767c85ef8b5d17e
losmescaleros <losmescaleros@gmail.com>
2014-04-13 17:52:50 -0400

    Added previous vaccinations for student in init script

--- ReasonableCareTest/db/init.sql
+++ ReasonableCareTest/db/init.sql
@@ -1,10 +1,10 @@
 VAR john_id NUMBER
 INSERT INTO Doctor(doctorName, password, phoneNumber, specialization)
-VALUES('John Terry', 'password', '919-100-2101', 'Oncology Surgery')
+VALUES('John Terry', 'password', '919-100-2101', 'Oncology Surgeon')
 RETURNING doctorId INTO :john_id;
 
 INSERT INTO Doctor(doctorName, password, phoneNumber, specialization)
-VALUES('Mary Jobs', 'password', '919-500-1212', 'Oncology Surgery');
+VALUES('Mary Jobs', 'password', '919-500-1212', 'Oncology Surgeon');
 
 INSERT INTO Nurse(nurseName, password) 
 VALUES('Rebecca Johnston', 'password');
@@ -40,5 +40,29 @@ TO_DATE('21.04.2014 16:00:00', 'dd.mm.yyyy hh24:mi:ss'),
 '', 100) RETURNING appointmentId INTO :appt_id;
 
 INSERT INTO MakesAppointment(studentId, doctorId, appointmentId)
+VALUES(:jason_id, :john_id, :appt_id);
+
+INSERT INTO Appointment(reasonForVisit, type, appointmentTime,
+doctorNotes, cost) VALUES('Vaccination', 'vaccination1',
+TO_DATE('16.03.2011 09:00:00', 'dd.mm.yyyy hh24:mi:ss'),
+'', 20) RETURNING appointmentId INTO :appt_id;
+
+INSERT INTO Appointment(studentId, doctorId, appointmentId)
+VALUES(:jason_id, :john_id, :appt_id);
+
+INSERT INTO Appointment(reasonForVisit, type, appointmentTime,
+doctorNotes, cost) VALUES('Vaccination', 'vaccination2',
+TO_DATE('16.03.2011 09:30:00', 'dd.mm.yyyy hh24:mi:ss'),
+'', 20) RETURNING appointmentId INTO :appt_id;
+
+INSERT INTO Appointment(studentId, doctorId, appointmentId)
+VALUES(:jason_id, :john_id, :appt_id);
+
+INSERT INTO Appointment(reasonForVisit, type, appointmentTime,
+doctorNotes, cost) VALUES('Vaccination', 'vaccination3',
+TO_DATE('16.03.2011 10:00:00', 'dd.mm.yyyy hh24:mi:ss'),
+'', 20) RETURNING appointmentId INTO :appt_id;
+
+INSERT INTO Appointment(studentId, doctorId, appointmentId)
 VALUES(:jason_id, :john_id, :appt_id);
 
